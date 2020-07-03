76b4d03029dc807a5487765caa9fbf5158529247
Bret Merritt <merrittbret9@gmail.com>
2020-07-03 14:10:22 -0600

    Add src/order.js and test/order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,45 @@
 
+function takeOrder(incomingOrder, orderList) {
+  if (orderList.length < 3) {
+    orderList.push(incomingOrder);
+    return orderList;
+  } else {
+
+  }
+}
+
+function refundOrder(orderKey, orderList) {
+  for (var i = 0; i < orderList.length; i++) {
+    if (orderList[i].orderNumber === orderKey) {
+      orderList.splice(i, 1);
+      return orderList;
+    }
+  }
+}
+
+function listItems(orderList) {
+  var itemList = [];
+    for (var i = 0; i < orderList.length; i++) {
+      itemList.push(orderList[i].item);
+    }
+  var itemListString = itemList.join(", ");
+  return itemListString;
+}
+
+function searchOrder(orderList, item) {
+  var orderCheck = false;
+  for (i = 0; i < orderList.length; i++) {
+    if (orderList[i].item === item) {
+      orderCheck = true;
+    } else {
+    }
+  }
+  return orderCheck;
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
