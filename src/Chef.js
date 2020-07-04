2047b7e2ca6153f661d077c6d35c19a295833954
Bret Merritt <merrittbret9@gmail.com>
2020-07-04 13:47:39 -0600

    Add restaurant.js, restaurant-test.js, Chef.js, and Chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,25 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+
+  greetCustomer(customer, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customer}!`;
+    } else {
+      return `Hello, ${customer}!`;
+    }
+  }
+
+  checkForFood(item) {
+    if (this.restaurant.menus[item.type].includes(item)) {
+      return `Yes, we're serving ${item.name} today!`;
+    } else {
+      return `Sorry, we aren't serving ${item.name} today.`;
+    }
+  }
+
 }
 
 module.exports = Chef;
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,50 @@
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return restaurant;
+}
+
+function addMenuItem(restaurant, item) {
+    if (restaurant.menus[item.type].includes(item)) {
+    } else {
+      restaurant.menus[item.type].push(item);
+      return restaurant;
+    }
+}
+
+
+
+function removeMenuItem(restaurant, itemName, type) {
+  function findItemByName(item) {
+    if (item.name === itemName) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  if (type === undefined) {
+    return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+    } else {
+    var indexOfItemToDelete = restaurant.menus[type].findIndex(findItemByName);
+    if (indexOfItemToDelete === -1) {
+      return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+    } else {
+      restaurant.menus[type].splice(indexOfItemToDelete, 1);
+      return `No one is eating our ${itemName} - it has been removed from the ${type} menu!`;
+    }
+  }
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
